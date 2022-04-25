5b2560ebd630130c907077f826419ebce1f103b8
ArekK <arkadiusz.koszewski@frey.pl>
2022-04-25 21:32:56 +0200

    Szkielet

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <list>
+
+class Record
+{
+public:
+    Record(std::string title, std::string date, std::string description, int id = -1)
+      : title(std::move(title)), date(std::move(date)), description(std::move(description)), id(id)
+    {}
+    void print() const
+    {
+        std::cout << "Data: " << date << " Zadanie: " << title  << " Opis: " << description << std::endl;
+    }
+private:
+    int id;
+    std::string title;
+    std::string date;
+    std::string description;
+};
+
+class DbManager
+{
+public:
+    DbManager(std::string path);
+    std::list<Record> getRecords() const;
+    void insert(Record record);
+private:
+    std::string path;
+};
+
+DbManager::DbManager(std::string path)
+  : path(std::move(path))
+{}
+
+std::list<Record> DbManager::getRecords() const
+{
+    // auto patients = db.executeQuery<Patient>("select * from patients", {sizeof(Patient::id), 20, 20, 12});
+    std::list<Record> records;
+    records.emplace_back("Domyslny record", "2000-11-11", "Blabla");
+    return records;
+}
+
+void DbManager::insert(Record record)
+{
+    // std::string komenda = "INSERT INTO Patients (name, surname, pesel) VALUES ('"+patient.name+ "','" +patient.surname+"','"+patient.pesel+"');";
+    // db.executeQuery<>(komenda);
+}
+
+Record createRecordFromUser()
+{
+    std::string date, title, desc;
+    std::cout << "Podaj date: ";
+    std::cin >> date;
+    std::cout << "Podaj tytul zadania: ";
+    std::cin >> title;
+    std::cout << "Dokladniejszy opis: ";
+    std::cin >> desc;
+    return Record(date, title, desc);
+}
+
+int main()
+{
+    DbManager db("Lab1 db");
+
+    while (true) {
+        char option;
+        std::cout << "\nCo chcesz zrobic?" << std::endl
+                << "1 - Insert\n2 - Select\n3 - Wyjscie" << std::endl;
+        std::cin >> option;
+        if (option == '1') {
+            Record record = createRecordFromUser();
+            db.insert(record);
+            std::cout << "Dodano rekord: " << std::endl;
+            record.print();
+        }
+        else if (option == '2') {
+            std::list<Record> records = db.getRecords();
+            for (const auto& record : records) {
+                record.print();
+            }
+        }
+        else {
+            break;
+        }
+    }
+    return 0;
+}
